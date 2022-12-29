b9b2a5e37396f66af21c868712325533491ed2e3
adarshvdoit <adarsh.vdoit@gmail.com>
2022-12-29 10:01:33 +0530

    HTML

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, Query, Path,HTTPException,status,Body
+from fastapi import FastAPI, Query, Path,HTTPException,status,Body,Request,Form
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel,Field
 from typing import Optional, List, Dict
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from database import cars
+from starlette.responses import HTMLResponse
 from starlette.status import HTTP_400_BAD_REQUEST
-
+templates = Jinja2Templates(directory="templates")
 class Car(BaseModel):
     make: Optional[str]
     model: Optional[str]
@@ -16,29 +20,47 @@ class Car(BaseModel):
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return{"Welcome to fast API"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/cars", response_model=List[Dict[str,Car]])
-def get_cars(number:Optional[str]=Query("10",max_length=3)):
-    response =[]
+
+@app.get("/", response_class=RedirectResponse)
+def root(request: Request):
+    return RedirectResponse(url="/cars")
+
+@app.get("/cars", response_class=HTMLResponse)
+def get_cars(request: Request, number: Optional[str] = Query("10",max_length=3)):
+    response = []
     for id, car in list(cars.items())[:int(number)]:
-        to_add={}
-        to_add[id]=car
-        response.append(to_add)
-    return response
+        response.append((id,car))
+    return templates.TemplateResponse("index.html", {"request": request, "cars": response, "title": "Home"})
 
+@app.post("/search", response_class=RedirectResponse)
+def search_cars(id: str = Form(...)):
+    return RedirectResponse("/cars/" + id, status_code=302)
 
-@app.get("/cars/{id}", response_model=Car)
-def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
+@app.get("/cars/{id}", response_class=HTMLResponse)
+def get_car_by_id(request:Request,id: int = Path(...,ge=0,lt=1000)):
     car = cars.get(id)
     if not car:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Could not found")
-    return car
+        templates.TemplateResponse("search.html", {"request":request,"car":car,"id":id},status_code=status.HTTP_404_NOT_FOUND )
+    return templates.TemplateResponse("search.html", {"request":request,"car":car,"id":id},status_code=status.HTTP_200_OK )
+
+@app.get("/create", response_class=HTMLResponse)
+def create_car(request: Request):
+    return templates.TemplateResponse("create.html", {"request": request, "title": "Create Car"})
+
 
 @app.post("/cars", status_code=status.HTTP_201_CREATED)
-def add_cars(body_cars:List[Car],min_id:Optional[int]=Body(0)):
+def add_cars(
+    make: Optional[str] = Form(...),
+    model: Optional[str] = Form(...),
+    year: Optional[str] = Form(...),
+    price: Optional[float] = Form(...),
+    engine: Optional[str] = Form(...),
+    autonomous: Optional[bool] = Form(...),
+    sold: Optional[List[str]] = Form(None),
+    min_id: Optional[int] = Body(0)):
+    body_cars = [Car(make=make,model=model,year=year,price=price,engine=engine,autonomous=autonomous,sold=sold)]
     if len(body_cars) < 1:
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST,detail="No cars to add.")
     min_id = len(cars.values()) + min_id
@@ -47,24 +69,40 @@ def add_cars(body_cars:List[Car],min_id:Optional[int]=Body(0)):
             min_id += 1
         cars[min_id] = car
         min_id += 1
-        
+    return RedirectResponse(url="/cars", status_code=302)
 
-@app.put("/cars/{id}",response_model=Dict[str,Car])
-def update_car(id:int,car:Car =Body(...)):
+@app.get("/edit", response_class=HTMLResponse)
+def edit_car(request: Request, id: int = Query(...)):
+    car = cars.get(id)
+    if not car:
+        return templates.TemplateResponse("search.html", {"request": request, "id": id, "car": car, "title": "Edit Car"}, status_code=status.HTTP_404_NOT_FOUND)
+    return templates.TemplateResponse("edit.html", {"request": request, "id": id, "car": car, "title": "Edit Car"})
+
+@app.post("/cars/{id}")
+def update_car(request: Request, id: int,
+    make: Optional[str] = Form(None),
+    model: Optional[str] = Form(None),
+    year: Optional[str] = Form(None),
+    price: Optional[float] = Form(None),
+    engine: Optional[str] = Form(None),
+    autonomous: Optional[bool] = Form(None),
+    sold: Optional[List[str]] = Form(None),):
     stored = cars.get(id)
     if not stored:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could Not Found")
-    stored = Car(**stored)
+        return templates.TemplateResponse("search.html", {"request": request, "id": id, "car": stored, "title": "Edit Car"}, status_code=status.HTTP_404_NOT_FOUND)
+    stored = Car(**dict(stored))
+    car = Car(make=make,model=model,year=year,price=price,engine=engine,autonomous=autonomous,sold=sold)
     new = car.dict(exclude_unset=True)
     new = stored.copy(update=new)
-    cars[id]=jsonable_encoder(new)
-    response={}
-    response[id]=cars[id]
-    return response
+    cars[id] = jsonable_encoder(new)
+    response = {}
+    response[id] = cars[id]
+    return RedirectResponse(url="/cars", status_code=302)
 
-@app.delete("/cars/{id}")
-def delete_car(id:int):
+@app.get("/delete/{id}", response_class=RedirectResponse)
+def delete_car(request: Request, id: int = Path(...)):
     if not cars.get(id):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could Not Found")
+        return templates.TemplateResponse("search.html", {"request": request, "id": id, "title": "Edit Car"}, status_code=status.HTTP_404_NOT_FOUND)
     del cars[id]
+    return RedirectResponse(url="/cars")
 
